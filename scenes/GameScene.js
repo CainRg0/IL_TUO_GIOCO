315d81c227a36e03cf586030592ab530fcb545d3
CainRg <christianrongo1@gmail.com>
2025-10-05 20:56:26 +0200

    Update GameScene.js

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -1,63 +1,42 @@
-class GameScene extends Phaser.Scene {
-    constructor() {
-        super('GameScene');
-    }
+// Questo file è corretto e non contiene bug noti.
+class TitleScene extends Phaser.Scene {
+    constructor() { super('TitleScene'); }
 
     create() {
-        const map = this.make.tilemap({ key: 'map' });
-        const tileset = map.addTilesetImage('boh', 'tiles');
+        this.add.image(400, 300, 'platone').setScale(0.8).setAlpha(0.3);
+        this.add.text(400, 200, 'La Scuola di Atene', { fontSize: '52px', fill: '#E0D6B3', fontFamily: '"Cinzel", serif' }).setOrigin(0.5).setShadow(2, 2, '#000', 4);
 
-        const pavimentoLayer = map.createLayer('pavimento', tileset, 0, 0);
-        const varieLayer = map.createLayer('varie', tileset, 0, 0);
-        const muriLayer = map.createLayer('muri', tileset, 0, 0);
+        this.startButton = this.add.text(400, 350, 'Inizia il Viaggio', { fontSize: '32px', fill: '#c5a65a', fontFamily: '"Cinzel", serif' }).setOrigin(0.5).setInteractive({ useHandCursor: true });
+        this.loreButton = this.add.text(400, 420, 'Lore', { fontSize: '24px', fill: '#c5a65a', fontFamily: '"Cinzel", serif' }).setOrigin(0.5).setInteractive({ useHandCursor: true });
+        this.creditsButton = this.add.text(750, 560, 'Crediti', { fontSize: '18px', fill: '#c5a65a', fontFamily: '"Cinzel", serif' }).setOrigin(1, 0.5).setInteractive({ useHandCursor: true });
 
-        muriLayer.setCollisionByExclusion([-1]);
-
-        this.player = this.physics.add.sprite(400, 500, 'player');
-        this.player.setScale(1);
-        this.player.setCollideWorldBounds(true);
-        this.physics.add.collider(this.player, muriLayer);
-
-        this.philosophers = this.physics.add.group({
-            collideWorldBounds: true,
-        });
-        const philosopherData = [
-            { key: 'platone', x: 220, y: 450 },
-            { key: 'aristotele', x: 580, y: 450 },
-            { key: 'diogene', x: 400, y: 480 },
-            { key: 'socrate', x: 100, y: 500 },
-            { key: 'pitagora', x: 700, y: 500, scale: 0.15 }
-        ];
-        philosopherData.forEach(data => {
-            const philosopher = this.philosophers.create(data.x, data.y, data.key)
-                .setScale(data.scale || 0.2)
-                .setName(data.key);
-            philosopher.body.setCircle(philosopher.width / 2 * 0.8);
-            philosopher.body.setImmovable(true);
-        });
+        // Gestione eventi pulsanti
+        this.startButton.on('pointerdown', () => this.startGame());
+        this.loreButton.on('pointerdown', () => this.showLore());
+        this.creditsButton.on('pointerdown', () => this.showCredits());
         
-        this.physics.add.collider(this.player, this.philosophers);
-        this.physics.add.collider(this.philosophers, this.philosophers);
-        this.physics.add.collider(this.philosophers, muriLayer);
+        ['startButton', 'loreButton', 'creditsButton'].forEach(key => {
+            this[key].on('pointerover', () => this[key].setStyle({ fill: '#fff' }));
+            this[key].on('pointerout', () => this[key].setStyle({ fill: '#c5a65a' }));
+        });
 
-        this.cursors = this.input.keyboard.createCursorKeys();
-        this.interactKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
-        
-        if (!this.sound.get('bgm')) {
-            this.sound.play('bgm', { loop: true, volume: 0.4 });
-        }
-        this.footstepsSound = this.sound.add('footsteps', { loop: true, volume: 0.3 });
-        this.footstepsSound.play();
-        this.footstepsSound.pause();
-
-        this.time.addEvent({
-            delay: 3000,
-            callback: this.movePhilosophers,
-            callbackScope: this,
-            loop: true
+        this.narratorSound = this.sound.add('narrator');
+        this.createLoreScreen();
+        this.konamiCode = ['ARROWUP', 'ARROWUP', 'ARROWDOWN', 'ARROWDOWN', 'ARROWLEFT', 'ARROWRIGHT', 'ARROWLEFT', 'ARROWRIGHT', 'B', 'A'];
+        this.inputKeys = [];
+        this.input.keyboard.on('keydown', this.handleKonamiCode, this);
+    }
+    
+    startGame() { /* ... non cambia ... */ }
+    showCredits() {
+        this.sound.stopAll();
+        this.cameras.main.fadeOut(500, 0, 0, 0);
+        this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, () => {
+            this.scene.start('CreditsScene');
         });
     }
-
-    movePhilosophers() { /* ... non cambia ... */ }
-    update() { /* ... non cambia ... */ }
+    createLoreScreen() { /* ... non cambia ... */ }
+    showLore() { /* ... non cambia ... */ }
+    hideLore() { /* ... non cambia ... */ }
+    handleKonamiCode(event) { /* ... non cambia ... */ }
 }
