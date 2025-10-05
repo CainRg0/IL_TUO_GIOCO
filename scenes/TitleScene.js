0711a5db50596760441b837f4b6ab8375348a121
CainRg <christianrongo1@gmail.com>
2025-10-05 14:43:04 +0200

    Update TitleScene.js

--- scenes/TitleScene.js
+++ scenes/TitleScene.js
@@ -1,19 +1,17 @@
-// File: scenes/TitleScene.js
-
 class TitleScene extends Phaser.Scene {
     constructor() {
         super('TitleScene');
     }
 
     create() {
-        // Aggiungiamo uno sfondo per il menu
-        this.add.image(400, 300, 'platone').setScale(0.8).setAlpha(0.3); // Usiamo l'immagine di Platone come sfondo scuro
+        // Sfondo per il menu
+        this.add.image(400, 300, 'platone').setScale(0.8).setAlpha(0.3);
 
         // Titolo del gioco
         this.add.text(400, 200, 'La Scuola di Atene', {
             fontSize: '52px',
-            fill: '#E0D6B3', // Stesso colore oro usato prima
-            fontFamily: '"Cinzel", serif' // Usiamo lo stesso font del menu HTML
+            fill: '#E0D6B3',
+            fontFamily: '"Cinzel", serif'
         }).setOrigin(0.5).setShadow(2, 2, '#000', 4);
 
         // Pulsante di avvio
@@ -33,7 +31,6 @@ class TitleScene extends Phaser.Scene {
 
         // Azione al click sul pulsante
         startButton.on('pointerdown', () => {
-            // Aggiungiamo una transizione in dissolvenza per un effetto più pulito
             this.cameras.main.fadeOut(500, 0, 0, 0);
             this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, () => {
                 this.scene.start('GameScene');
