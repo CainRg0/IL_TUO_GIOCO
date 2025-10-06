9fb1daa47284e31c585e922101fbc399e4d9f9a0
CainRg <christianrongo1@gmail.com>
2025-10-06 12:57:07 +0200

    Update CreditsScene.js

--- scenes/CreditsScene.js
+++ scenes/CreditsScene.js
@@ -1,69 +1,26 @@
 class CreditsScene extends Phaser.Scene {
-    constructor() {
-        super('CreditsScene');
-    }
-
+    constructor() { super('CreditsScene'); }
     create() {
         this.sound.stopAll();
         this.sound.play('credits_music', { loop: true, volume: 0.5 });
         this.add.image(400, 300, 'scuola_di_atene').setScale(0.7).setAlpha(0.4);
-
         const creditsText = [
-            'La Scuola di Atene',
-            '',
-            '',
-            'Un Progetto Realizzato Da:',
-            '',
-            '',
-            'Christian Rongo',
-            '[ Game Developer ]',
-            '',
-            'Francesco Maffettone',
-            '[ Concept Creator ]',
-            '',
-            'Pasquale Muriello',
-            '[ Art Designer ]',
-            '',
-            '',
-            '',
-            'Un Ringraziamento Speciale ai filosofi:',
-            '',
-            'Platone',
-            'Aristotele',
-            'Diogene',
-            'Socrate',
-            'Pitagora',
-            '',
-            '',
-            '',
-            'Hanno partecipato al corso:',
-            '',
-            'Panico Christian',
-            'Valerio D\'Alconzo',
-            'Palladino Gabriele',
-            'Daniele Napolitano',
-            'Fusaro Mario',
-            'Zito Giovanni',
-            'Luca Lombardi',
-            'Manila Signore',
-            'Adriano Gabriele',
-            'Alessandro de Falco',
-            'Testa Daniele',
-            'Giuseppe Di Mauro',
-            'Marco Aprea',
-            '',
-            '',
+            'La Scuola di Atene', '', '', 'Un Progetto Realizzato Da:', '', '',
+            'Christian Rongo', '[ Game Developer ]', '',
+            'Francesco Maffettone', '[ Concept Creator ]', '',
+            'Pasquale Muriello', '[ Art Designer ]', '', '', '',
+            'Un Ringraziamento Speciale ai Pensatori:', '',
+            'Platone', 'Aristotele', 'Diogene', 'Socrate', 'Pitagora', '', '', '',
+            'Hanno partecipato al gruppo:', '',
+            'Panico Christian', 'Valerio D\'Alconzo', 'Palladino Gabriele', 'Daniele Napolitano',
+            'Fusaro Mario', 'Zito Giovanni', 'Luca Lombardi', 'Manila Signore',
+            'Adriano Gabriele', 'Alessandro de Falco', 'Testa Daniele', '', '',
             'Grazie per aver giocato!',
         ];
-
         const textObject = this.add.text(400, 600, creditsText, {
-            fontSize: '28px',
-            fill: '#E0D6B3',
-            fontFamily: '"Cinzel", serif',
-            align: 'center',
-            lineSpacing: 15
+            fontSize: '28px', fill: '#E0D6B3', fontFamily: '"Cinzel", serif',
+            align: 'center', lineSpacing: 15
         }).setOrigin(0.5, 0);
-
         this.tweens.add({
             targets: textObject,
             y: -textObject.height,
@@ -78,5 +35,3 @@ class CreditsScene extends Phaser.Scene {
         });
     }
 }
-
-
