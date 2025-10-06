c8d9bd629977c0225f0e0053e6195857bb890c8f
CainRg <christianrongo1@gmail.com>
2025-10-06 18:35:31 +0200

    Update IntroScene.js

--- scenes/IntroScene.js
+++ scenes/IntroScene.js
@@ -1,18 +1,13 @@
 class IntroScene extends Phaser.Scene {
-    constructor() {
-        super('IntroScene');
-    }
+    constructor() { super('IntroScene'); }
 
     create() {
         this.cameras.main.setBackgroundColor('#000000');
         this.cameras.main.fadeIn(500);
-
         const dialogBox = this.add.graphics();
         dialogBox.fillStyle(0x000000, 0.8).fillRoundedRect(50, 400, 700, 180, 16);
         this.dialogText = this.add.text(80, 420, '', { fontSize: '20px', fill: '#fff', wordWrap: { width: 640 } });
-
         const introText = 'Benvenuto, giovane pensatore.\n\nIl tuo viaggio nella conoscenza sta per iniziare. Esplora questo luogo e parla con i grandi maestri del passato. Supera le loro prove per dimostrare la tua saggezza.\n\n(Clicca per continuare)';
-
         this.typewriteText(introText);
     }
 
@@ -20,7 +15,6 @@ class IntroScene extends Phaser.Scene {
         const length = text.length;
         let i = 0;
         this.dialogText.setText('');
-        
         this.time.addEvent({
             callback: () => {
                 this.dialogText.text += text[i];
