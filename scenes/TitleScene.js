79bb05cfe705298f719200d9e4a046beb4f5c190
CainRg <christianrongo1@gmail.com>
2025-10-06 16:06:29 +0200

    Update TitleScene.js

--- scenes/TitleScene.js
+++ scenes/TitleScene.js
@@ -7,7 +7,7 @@ class TitleScene extends Phaser.Scene {
 
         this.add.image(400, 300, 'platone').setScale(0.8).setAlpha(0.3);
         this.add.text(400, 150, 'Paideia', { fontSize: '72px', fill: '#E0D6B3', fontFamily: '"Cinzel", serif' }).setOrigin(0.5).setShadow(2, 2, '#000', 4);
-        this.add.text(400, 230, '(Alla scuola di atene)', { fontSize: '36px', fill: '#E0D6B3', fontFamily: '"Cinzel", serif' }).setOrigin(0.5).setShadow(2, 2, '#000', 4);
+        this.add.text(400, 230, 'ALLA SCUOLA DI ATENE', { fontSize: '36px', fill: '#E0D6B3', fontFamily: '"Cinzel", serif' }).setOrigin(0.5).setShadow(2, 2, '#000', 4);
         
         this.startButton = this.add.text(400, 350, 'Inizia il Viaggio', { fontSize: '32px', fill: '#c5a65a', fontFamily: '"Cinzel", serif' }).setOrigin(0.5).setInteractive({ useHandCursor: true });
         this.loreButton = this.add.text(400, 420, 'Lore', { fontSize: '24px', fill: '#c5a65a', fontFamily: '"Cinzel", serif' }).setOrigin(0.5).setInteractive({ useHandCursor: true });
@@ -94,3 +94,4 @@ class TitleScene extends Phaser.Scene {
         }
     }
 }
+
