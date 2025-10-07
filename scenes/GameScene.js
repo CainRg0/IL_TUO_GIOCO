85dd146413f2c268ba0439794d65029cf90914c8
CainRg <christianrongo1@gmail.com>
2025-10-07 14:55:26 +0200

    Update GameScene.js

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -1,14 +1,11 @@
 class GameScene extends Phaser.Scene {
     constructor() { super('GameScene'); }
-
     create() {
         this.cameras.main.setBackgroundColor('#3d3d3d');
         this.cameras.main.fadeIn(500, 0, 0, 0);
-
         this.player = this.physics.add.sprite(100, 300, 'player');
         this.player.setCollideWorldBounds(true);
         this.player.setScale(0.1);
-
         this.philosophers = this.physics.add.group({ collideWorldBounds: true });
         const philosopherData = [
             { key: 'platone', x: 150, y: 150, scale: 0.2 },
@@ -18,37 +15,23 @@ class GameScene extends Phaser.Scene {
             { key: 'pitagora', x: 400, y: 300, scale: 0.15 }
         ];
         philosopherData.forEach(data => {
-            const philosopher = this.philosophers.create(data.x, data.y, data.key)
-                .setScale(data.scale)
-                .setName(data.key);
+            const philosopher = this.philosophers.create(data.x, data.y, data.key).setScale(data.scale).setName(data.key);
             philosopher.body.setCircle(philosopher.width / 2 * 0.8);
             philosopher.body.setImmovable(true);
             const name = data.key.charAt(0).toUpperCase() + data.key.slice(1);
             const label = this.add.text(philosopher.x, philosopher.y - 45, name, { fontSize: '14px', fill: '#ffffff', fontFamily: '"Cinzel", serif', stroke: '#000000', strokeThickness: 3 }).setOrigin(0.5);
             philosopher.nameLabel = label;
         });
-        
         this.physics.add.collider(this.player, this.philosophers);
         this.physics.add.collider(this.philosophers, this.philosophers);
-        
         this.cursors = this.input.keyboard.createCursorKeys();
         this.interactKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
-
-        if (!this.sound.get('bgm')) {
-            this.sound.play('bgm', { loop: true, volume: 0.4 });
-        }
+        if (!this.sound.get('bgm')) { this.sound.play('bgm', { loop: true, volume: 0.4 }); }
         this.footstepsSound = this.sound.add('footsteps', { loop: true, volume: 0.3 });
         this.footstepsSound.play();
         this.footstepsSound.pause();
-
-        this.time.addEvent({
-            delay: 3000,
-            callback: this.movePhilosophers,
-            callbackScope: this,
-            loop: true
-        });
+        this.time.addEvent({ delay: 3000, callback: this.movePhilosophers, callbackScope: this, loop: true });
     }
-
     movePhilosophers() {
         if (this.dialogActive) return;
         const speed = 30;
@@ -63,53 +46,34 @@ class GameScene extends Phaser.Scene {
             }
         });
     }
-
     update() {
         this.philosophers.getChildren().forEach(philosopher => {
             if (philosopher.nameLabel) {
                 let labelX = philosopher.x;
                 const labelWidth = philosopher.nameLabel.width / 2;
-                if (labelX - labelWidth < 0) {
-                    labelX = labelWidth;
-                } else if (labelX + labelWidth > this.physics.world.bounds.width) {
-                    labelX = this.physics.world.bounds.width - labelWidth;
-                }
+                if (labelX - labelWidth < 0) labelX = labelWidth;
+                else if (labelX + labelWidth > this.physics.world.bounds.width) labelX = this.physics.world.bounds.width - labelWidth;
                 philosopher.nameLabel.setPosition(labelX, philosopher.y - 45);
             }
         });
-
-        if (this.dialogActive) {
-            this.player.setVelocity(0);
-            this.philosophers.setVelocity(0, 0);
-            if (!this.footstepsSound.isPaused) this.footstepsSound.pause();
-            return;
-        }
-
+        if (this.dialogActive) { this.player.setVelocity(0); this.philosophers.setVelocity(0, 0); if (!this.footstepsSound.isPaused) this.footstepsSound.pause(); return; }
         const playerSpeed = 200;
         this.player.setVelocity(0);
-
         if (this.cursors.left.isDown) this.player.setVelocityX(-playerSpeed);
         else if (this.cursors.right.isDown) this.player.setVelocityX(playerSpeed);
         if (this.cursors.up.isDown) this.player.setVelocityY(-playerSpeed);
         else if (this.cursors.down.isDown) this.player.setVelocityY(playerSpeed);
-
         const isMoving = this.player.body.velocity.length() > 0;
         if (isMoving && this.footstepsSound.isPaused) this.footstepsSound.resume();
         else if (!isMoving && !this.footstepsSound.isPaused) this.footstepsSound.pause();
-
         let canInteractWith = null;
         for (const philosopher of this.philosophers.getChildren()) {
-            const distance = Phaser.Math.Distance.Between(this.player.x, this.player.y, philosopher.x, philosopher.y);
-            if (distance < 100) {
+            if (Phaser.Math.Distance.Between(this.player.x, this.player.y, philosopher.x, philosopher.y) < 100) {
                 canInteractWith = philosopher;
                 break;
             }
         }
         this.events.emit('interactionUpdate', canInteractWith);
-
-        if (canInteractWith && Phaser.Input.Keyboard.JustDown(this.interactKey)) {
-            this.dialogActive = true;
-            this.events.emit('startDialog', canInteractWith.name);
-        }
+        if (canInteractWith && Phaser.Input.Keyboard.JustDown(this.interactKey)) { this.dialogActive = true; this.events.emit('startDialog', canInteractWith.name); }
     }
 }
