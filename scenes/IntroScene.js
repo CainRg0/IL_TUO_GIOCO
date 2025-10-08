5129d72a5162fbf91126aaf0a3efb55e0f5becd7
CainRg <christianrongo1@gmail.com>
2025-10-08 17:47:22 +0200

    Update IntroScene.js

--- scenes/IntroScene.js
+++ scenes/IntroScene.js
@@ -1,24 +1,42 @@
 class IntroScene extends Phaser.Scene {
-    constructor() { super('IntroScene'); }
+    constructor() {
+        super('IntroScene');
+    }
+
     create() {
+        // Avvia la musica dell'introduzione
+        this.sound.play('intro_music', { loop: true, volume: 0.6 });
+
         this.cameras.main.setBackgroundColor('#000000');
         this.cameras.main.fadeIn(500);
+
         const dialogBox = this.add.graphics();
-        dialogBox.fillStyle(0x000000, 0.8).fillRoundedRect(50, 400, 700, 180, 16);
-        this.dialogText = this.add.text(80, 420, '', { fontSize: '20px', fill: '#fff', wordWrap: { width: 640 } });
-        const introText = 'Benvenuto, giovane pensatore.\n\nIl tuo viaggio nella conoscenza sta per iniziare. Esplora questo luogo e parla con i grandi maestri del passato. Supera le loro prove per dimostrare la tua saggezza.\n\n(Clicca per continuare)';
+        dialogBox.fillStyle(0x000000, 0.8).fillRoundedRect(50, 380, 700, 200, 16); // Leggermente più grande
+        this.dialogText = this.add.text(80, 400, '', { fontSize: '20px', fill: '#fff', wordWrap: { width: 640 } });
+
+        // --- TESTO AGGIORNATO CON I COMANDI ---
+        const introText = 'Benvenuto, giovane pensatore.\n\n' +
+                          'Il tuo viaggio nella conoscenza sta per iniziare. Supera le prove dei grandi maestri del passato per dimostrare la tua saggezza.\n\n' +
+                          'COMANDI:\n' +
+                          '- Usa le FRECCE DIREZIONALI per muoverti.\n' +
+                          '- Avvicinati a un filosofo e premi [E] per interagire.\n\n' +
+                          '(Clicca per iniziare)';
+
         this.typewriteText(introText);
     }
+
     typewriteText(text) {
         const length = text.length;
         let i = 0;
         this.dialogText.setText('');
+        
         this.time.addEvent({
             callback: () => {
                 this.dialogText.text += text[i];
                 i++;
                 if (i === length) {
                     this.input.once('pointerdown', () => {
+                        this.sound.stopAll(); // Ferma la musica dell'intro
                         this.cameras.main.fadeOut(500, 0, 0, 0);
                         this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, () => {
                             this.scene.start('GameScene');
